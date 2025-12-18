04d4ac390688fe813181c4c11173a149c91331e3
Andres Salzate <andresjgsalzate@gmail.com>
2025-12-18 21:33:51 +0000

    build production 181220250433PM

--- public/assets/index-A78eGDUd.js
+++
@@ -1,2 +0,0 @@
-import{t as s}from"./index-VpZ7bfF7.js";function c(e){const t=this,i={...t.data("settings"),...e};t.compiler=n;function n(o){return s(o,i)}}export{c as default};
-//# sourceMappingURL=index-A78eGDUd.js.map
